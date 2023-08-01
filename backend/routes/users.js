d91e614ec08bb103bbc9f01939841916a7757ce5
Meet Guleria <meetguleria94@gmail.com>
2023-08-01 14:41:41 -0700

    documentation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/db');
+const bcrypt = require('bcrypt');
 
-router.post('/register', (req, res) => {
-    res.send('Register route');
-})
+router.post('/register', async (req, res) => {
+    //TODO: input validation will come here
+
+    //checking if user exits
+    const user = await db.oneOrNone('SELECT * FROM users WHERE email = $1', [req.body.email]);
+    if (user) {
+        return res.status(400).send('User with this email already exists')
+    }
+
+    //Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //Insert the new user into the database
+    const newUser = await db.one('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', 
+    [req.body.name, req.body.email, hashedPassword]);
+
+    //TODO: Create and send a JWT
+
+    res.send(newUser);
+});
 
 router.post('/login', (req, res) => {
-    res.send('Login route');
-})
+
+});
 
 module.exports = router;
