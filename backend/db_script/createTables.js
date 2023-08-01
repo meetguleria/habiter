f922475388e284d334be4abb26b98884253798c5
Meet Guleria <meetguleria94@gmail.com>
2023-08-01 15:47:32 -0700

    user-registration and login

---
+++ backend/db_script/createTables.js
@@ -0,0 +1,35 @@
+const db = require('../config/db');
+
+async function createTables() {
+    await db.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password VARCHAR(100) NOT NULL
+        )`
+    );
+
+    await db.query(`
+        CREATE TABLE IF NOT EXISTS habits (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            start_data DATE,
+            end_date DATE,
+            user_id INTEGER REFERENCES users(id)
+        )
+    `);
+
+    await db.query(`
+        CREATE TABLE IF NOT EXISTS habit_records (
+            id SERIAL PRIMARY KEY,
+            date DATE NOT NULL,
+            status BOOLEAN NOT NULL,
+            habit_id INTEGER REFERENCES habits(id)
+        )
+    `);
+
+    console.log('Tables created successfully');
+}
+
+createTables().catch(console.error)
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+
+//Schema for user data
+const schema = Joi.object({
+    name: Joi.string().min(1).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required()
+});
 
 router.post('/register', async (req, res) => {
-    //TODO: input validation will come here
+    //Validate user's input
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
 
     //checking if user exits
     const user = await db.oneOrNone('SELECT * FROM users WHERE email = $1', [req.body.email]);
@@ -25,8 +38,28 @@ router.post('/register', async (req, res) => {
     res.send(newUser);
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+    //Validate user's input
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
 
+    //check if user with provided email exists
+    const user = await db.oneOrNone('SELECT * FROM users WHERE email = $1', [req.body.email]);
+    if (!user) {
+        return res.status(400).send('Invalid email or password');
+    }
+
+    //Compare the password with the hashed password in the database
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+        return res.status(400).send('Invalid email or password');
+    }
+
+    //Create a JWT and send it to the client
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h'});
+    res.json({ token });
 });
 
-module.exports = router;
+module.exports = router;   
